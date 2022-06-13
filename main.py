71dd7c7d7cbb772552952b3e7e643df3703e45e4
Nova1924 <106910708+Nova1924@users.noreply.github.com>
2022-06-13 23:27:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import cv2
 import math
 
 
-img = cv2.imread('CVtask.jpg')
-imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+image = cv2.imread('CVtask.jpg')
+imgGrey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-cv2.imshow("img", img)
+cv2.imshow("img", image)
 
 
 
@@ -26,20 +26,21 @@ for i in range(4):
 
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
-    cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
-    x = approx.ravel()[0]
-    y = approx.ravel()[1] - 5
+    cv2.drawContours(image, [approx], 0, (0, 0, 0), 5)
+    a = approx.ravel()[0]
+    b = approx.ravel()[1] - 5
     if len(approx) == 4:
-        x1 ,y1, w, h = cv2.boundingRect(approx)
+        a1, b1, w, h = cv2.boundingRect(approx)
         aspectRatio = float(w)/h
-        asp1 = float(x1)/w
-        asp2 = float(y1)/h
+        asp1 = float(a1) / w
+        asp2 = float(b1) / h
         #print(aspectRatio)
         if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-            cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-            cv2.rectangle(img, (x1, y1) , (x1 + w, y1 + w), (0, 255, 0), 10)
-            x2 = x1+w
-            y2 = y1+h
-cv2.imshow("img", img)
+            cv2.putText(image, "square", (a, b), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+            cv2.rectangle(image, (a1, b1), (a1 + w, b1 + w), (0, 255, 0), 10)
+            x2 = a1 + w
+            y2 = b1 + h
+cv2.imshow("img", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+#there is not a final.png file in the repo sir because i was not able to find the cordinates of the corner to rotate and paste the image sorry sir
